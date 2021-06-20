f7cfc7df5af0accf8e0103b181a3d76e5e7db160
Yuto Ichihara <yutounwasese@gmail.com>
2021-06-20 22:00:45 +0900

    アイコン等の編集完了

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by yuto',
+  subtitle: 'プログラミング、英語、マーケティングについて発信してきます！',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
