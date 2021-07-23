d9cd2ec35f1bacfd04dece60979b353392da96c3
Yuto Ichihara <yutounwasese@gmail.com>
2021-07-23 14:31:20 +0900

    adのスクリプトを複数ページに追加

--- src/components/Post/Content/index.js
+++ src/components/Post/Content/index.js
@@ -1,2 +1,4 @@
 // @flow strict
 export { default } from './Content';
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1580241070744469"
+     crossorigin="anonymous"></script>
