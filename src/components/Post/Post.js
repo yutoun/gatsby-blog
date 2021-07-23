e9968bd77c2aacb1270c9200042f3e53086974e8
Yuto Ichihara <yutounwasese@gmail.com>
2021-07-23 14:31:20 +0900

    adのスクリプトを複数ページに追加

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -38,5 +38,6 @@ const Post = ({ post }: Props) => {
     </div>
   );
 };
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1580241070744469"
+     crossorigin="anonymous"></script>
 export default Post;
